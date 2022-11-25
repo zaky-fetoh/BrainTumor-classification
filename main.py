bc27a910e1973917caba574feaea81645f965913
Zaky fetoh <zaky.fetoh@gmail.com>
2022-11-25 04:21:45 +0200

    save and load epoch statistics

--- main.py
+++ main.py
@@ -13,11 +13,3 @@ preprocess = transforms.Compose([
 
 data = dorg.getloaders(preprocess,1)
 
-for dt in data:
-    for img, label in dt:
-        img = img.detach().numpy()* 255
-        img.shape = (224,224)
-        print(img.shape, type(img), img.min(), img.max())
-        plt.imshow(np.array(img, dtype= np.int))
-        plt.show()
-        plt.pause(1)
