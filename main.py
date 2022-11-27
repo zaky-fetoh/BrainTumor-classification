20de9da46153504019124d4c8d71e03afd878e3b
Zaky fetoh <zaky.fetoh@gmail.com>
2022-11-27 10:25:16 +0200

    Random Rotation is added also a vertical and horizontal flipings

--- main.py
+++ main.py
@@ -9,22 +9,24 @@ import data_org as dorg
 import model as mdl
 
 
-
 preprocess = transforms.Compose([
     transforms.ToPILImage(),
     transforms.Resize(256),
+    transforms.RandomRotation(20),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
 ])
 
-data = dorg.getloaders(preprocess,32)
+data = dorg.getloaders(preprocess, 32)
 
 opt = optim.Adam
-crit= nn.CrossEntropyLoss()
+crit = nn.CrossEntropyLoss()
 
 
 if __name__ == "__main__":
     prof = dict()
-    mdl.kfoldTraining(loaders=data,profile=prof,
+    mdl.kfoldTraining(loaders=data, profile=prof,
                       criterion=crit, optClass=opt,
-                      ustep= 128)
+                      ustep=128)
